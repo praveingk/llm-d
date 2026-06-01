2d1e8964cab2de53f48b20a2c910f9a32ebb5632
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2026-06-01 16:06:02 +0530

    Update instructions and version of UCCL
    Signed-off-by: Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>

--- docker/scripts/common/build-uccl.sh
+++ docker/scripts/common/build-uccl.sh
@@ -9,13 +9,8 @@ set -Eeux
 # - UCCL_PREFIX: installation prefix for UCCL libraries and headers
 #
 # Optional environment variables:
-# - UCCL_TRANSPORT: transport backend to use (default: rdma)
-#   Options: rdma, tcp, tcpx
-# - ENABLE_EFA: set to "true" to build with EFA transport instead (default: false)
 # - UCCL_DEVICE: device target (default: cuda). Options: cuda, rocm
 
-UCCL_TRANSPORT="${UCCL_TRANSPORT:-rdma}"
-ENABLE_EFA="${ENABLE_EFA:-false}"
 UCCL_DEVICE="${UCCL_DEVICE:-cuda}"
 
 cd /tmp
@@ -47,25 +42,7 @@ else
     MAKEFILE="Makefile"
 fi
 
-# Transport selection and build
-# ENABLE_EFA takes precedence over UCCL_TRANSPORT
-if [ "${ENABLE_EFA}" = "true" ]; then
-    USE_EFA=1 make -f "${MAKEFILE}" -j
-else
-    case "${UCCL_TRANSPORT}" in
-      tcp)
-        USE_TCP=1 make -f "${MAKEFILE}" -j
-        ;;
-      tcpx)
-        USE_TCPX=1 make -f "${MAKEFILE}" -j
-        ;;
-      *)
-        make -f "${MAKEFILE}" -j
-        ;;
-    esac
-fi
-
-PREFIX="${UCCL_PREFIX}" make -f "${MAKEFILE}" install
+make -f "${MAKEFILE}" install
 
 cd /tmp
 rm -rf uccl
